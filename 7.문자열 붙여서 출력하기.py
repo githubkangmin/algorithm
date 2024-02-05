93be4f35e9e0dbbfb1880eab7c14116dd334e82b
shyricecake <leelucky1@naver.com>
2024-02-05 19:46:29 +0900

    feat: 7.문자열 붙여서 출력하기

---
+++ 7.문자열 붙여서 출력하기.py
@@ -0,0 +1,3 @@
+str1, str2 = input().strip().split(' ')
+print(str1,str2,sep="")
+#코드업에서 푼 유형
--- 대소문자 바꿔서 출력하기.py
+++ 대소문자 바꿔서 출력하기.py
@@ -1,7 +1,8 @@
 str = input()
 
 for i in str:
-    if  i.isupper == True:
+    if  i.isupper() == True:
         print(i.lower())
     else:
         print(i.upper())
+# 안풀림 isupper뒤에 () 안적음 if 제대로 쓰는 거 처음임
